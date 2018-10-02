08ed74a2bd928802f4412030744d195736cda91e
Garry Clerge <clergeg@gmail.com>
2018-10-02 13:58:54 -0400

    started implementation of AR

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require ‘bundler’
+
+Bundler.require
+
+ActiveRecord::Base.establish_connection(adapter: ‘sqlite3’, database: ‘db/development.db’)
+ActiveRecord::Base.logger = false
+require_all ‘lib’
+require_all ‘app’
