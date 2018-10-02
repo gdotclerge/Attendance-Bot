98f63f6b0efdb96d1875ea1133ccfd692f79d107
Garry Clerge <clergeg@gmail.com>
2018-10-02 13:31:49 -0400

    added admin permission

--- Gemfile
+++ Gemfile
@@ -1,24 +1,8 @@
 source "https://rubygems.org"
 
 gem 'pry'
-
-source 'https://rubygems.org'
-
-
 gem 'google_drive'
 gem 'slack-ruby-bot'
 gem 'celluloid-io'
 gem 'json'
 gem 'figaro'
-
-# SLACK_API_TOKEN=xoxb-215587854228-431894811861-TwC0SpwhpxTWaBMao7iQgUHU bundle exec ruby command_application.rb
-
-
-# bot user token
-# SLACK_API_TOKEN=xoxb-2727337933-446149273952-NAk6MERcXT8GIEzSFbdyPyfu bundle exec ruby command_application.rb
-
-# xoxb-2727337933-446149273952-NAk6MERcXT8GIEzSFbdyPyfu
-
-
-# oauth
-# xoxp-2727337933-273611928977-446284850577-7f2d71e4b578138df20f435ba38e1320
--- command_application.rb
+++ command_application.rb
@@ -4,19 +4,58 @@ require 'slack-ruby-bot'
 require 'pry'
 
 
-class McColemanTesterBot < SlackRubyBot::Bot
+class ImHereBot < SlackRubyBot::Bot
+  @@session = GoogleSheet.from_service_account_key("config.json")
+
+
+
+  command 'admin present' do |client, data, match|
+    client.say(text: "awesome", channel: data.channel)
+  end
+
+  command 'admin init' do |client, data, match|
+    sheet_key = ImHereBot.spreadsheet_key(match)
+    slack_members = client.store.channels[data.channel].members
+  end
+
+  def self.spreadsheet_key(match)
+    str_arr = match["expression"].split("/")
+    index = str_arr.index("d") + 1
+    str_arr[index]
+  end
+
 
 
   command 'ping' do |client, data, match|
-    # binding.pry
+    binding.pry
     client.say(text: "awesome", channel: data.channel)
   end
 
   command 'present' do |client, data, match|
-    # binding.pry
-    put_cells(client.store.users[data.user]['real_name'])
+    # str_arr = match["expression"].split("/")
+    # index = str_arr.index("d") + 1
+    # sheet_code = str_arr[index]
+
+
+    # put_cells(client.store.users[data.user]['real_name'])
+    # McColemanTesterBot.is_admin(client)
+
+  end
+
+  operator "new class" do |client, data, match|
+    binding.pry
+  end
+
+
+  def self.permitted?(client, data, match)
+    admin_status = client.store.users[data.user].is_admin
+    ImHereBot.admin_command?(match) ? admin_status : true
+  end
+
+  def self.admin_command?(match)
+    match['command'].split(" ")[0] === "admin"
   end
 
 end
 
-McColemanTesterBot.run
+ImHereBot.run
