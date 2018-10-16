dc003ddd43b6343646faee2f05f139f8d48e1e91
Garry Clerge <clergeg@gmail.com>
2018-10-15 22:40:36 -0400

    updated google sheet to work with the correct date

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,9 @@
 require_relative '../config/environment'
 
-Dotenv.load('config/slack_credentials.env')
+Dotenv.load('config/.env')
 
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
 end
-
 
 ImHereBot.run
